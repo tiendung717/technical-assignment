53f68f8c54b5b986a541197d2113e9542e02833c
Dzung <dungnt.fet@gmail.com>
2026-09-09 09:46:36 +1200

    Add listed_as parameter defaulting to All

--- app/src/main/java/com/android/app/data/api/service/ApiService.kt
+++ app/src/main/java/com/android/app/data/api/service/ApiService.kt
@@ -5,12 +5,16 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val PHOTO_SIZE_LARGE = "Large"
+private const val LISTED_AS_ALL = "All"
+private const val LISTED_AS_AUCTIONS = "Auctions"
+private const val LISTED_AS_CLASSIFIED = "Classifieds"
 
 interface ApiService {
 
     @GET("listings/latest.json")
     suspend fun getLatestListings(
         @Query("rows") rows: Int,
-        @Query("photo_size") photoSize: String = PHOTO_SIZE_LARGE
+        @Query("photo_size") photoSize: String = PHOTO_SIZE_LARGE,
+        @Query("listed_as") listedAs: String = LISTED_AS_ALL
     ): LatestListingsResponse
 }
